fde5248b9f0c78c1434b0fe99e5f303fc4b28c2d
ivanloktionov <71848058+ivanloktionov@users.noreply.github.com>
2023-06-22 18:16:35 +0300

    Update kbot.go

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package cmd
 
@@ -10,12 +10,10 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
-	"github.com/stianeikeland/go-rpio"
 	telebot "gopkg.in/telebot.v3"
 )
 
 var (
-	// TeleToken bot
 	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
@@ -31,9 +29,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		fmt.Printf("kbot %s started", appVersion)
-
 		kbot, err := telebot.NewBot(telebot.Settings{
 			URL:    "",
 			Token:  TeleToken,
@@ -41,57 +37,18 @@ to quickly create a Cobra application.`,
 		})
 
 		if err != nil {
-			log.Fatalf("Plaese check TELE_TOKEN env variable. %s", err)
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 			return
 		}
 
-		err = rpio.Open()
-		if err != nil {
-			log.Printf("Unable to open gpio: %s", err.Error())
-		}
-
-		defer rpio.Close()
-
-		trafficSignal := make(map[string]map[string]int8)
-
-		trafficSignal["red"] = make(map[string]int8)
-		trafficSignal["amber"] = make(map[string]int8)
-		trafficSignal["green"] = make(map[string]int8)
-
-		trafficSignal["red"]["pin"] = 12
-		//default on/off
-		//trafficSignal["red"]["on"]=0
-		trafficSignal["amber"]["pin"] = 27
-		trafficSignal["green"]["pin"] = 22
-
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 
-			var (
-				err error
-				pin = rpio.Pin(0)
-			)
 			log.Print(m.Message().Payload, m.Text())
 			payload := m.Message().Payload
 
 			switch payload {
 			case "hello":
 				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
-
-			case "red", "amber", "green":
-				pin = rpio.Pin(trafficSignal[payload]["pin"])
-				if trafficSignal[payload]["on"] == 0 {
-					pin.Output()
-					trafficSignal[payload]["on"] = 1
-				} else {
-					pin.Input()
-					trafficSignal[payload]["on"] = 0
-				}
-
-				err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
-
-			default:
-				err = m.Send("Usage: /s red|amber|green")
-
 			}
 
 			return err
@@ -99,6 +56,7 @@ to quickly create a Cobra application.`,
 		})
 
 		kbot.Start()
+
 	},
 }
 
